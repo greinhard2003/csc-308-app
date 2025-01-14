2ab246455ca27841a1d8c315234446698f0aae8c
Garrett Reinhard <skyscreamer13@icloud.com>
2025-01-14 15:14:36 -0800

    id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -8,38 +8,39 @@ const users = {
     {
       id: "xyz789",
       name: "Charlie",
-      job: "Janitor"
+      job: "Janitor",
     },
     {
       id: "abc123",
       name: "Mac",
-      job: "Bouncer"
+      job: "Bouncer",
     },
     {
       id: "ppp222",
       name: "Mac",
-      job: "Professor"
+      job: "Professor",
     },
     {
       id: "yat999",
       name: "Dee",
-      job: "Aspring actress"
+      job: "Aspring actress",
     },
     {
       id: "zap555",
       name: "Dennis",
-      job: "Bartender"
-    }
-  ]
+      job: "Bartender",
+    },
+  ],
 };
-
-app.use(express.json());
 
 const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
+  return users["users_list"].filter((user) => user["name"] === name);
 };
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.use(express.json());
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
@@ -49,6 +50,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
